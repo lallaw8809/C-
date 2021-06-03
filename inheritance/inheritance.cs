748e36bb59a291c069bf2bbd7988348bd261167c
lallaw8809 <lal.lawrence@365id.com>
2021-06-03 14:44:12 +0200

    Added a sample code for inheritance

---
+++ inheritance/inheritance.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Program
+{
+    class Inheritance
+    {
+        // Base or Parent class
+        class Parent
+        {
+            // Class member
+            public int parentVariable = 10;
+
+            // Class method definition
+            public void parentClassMethod()
+            {
+                Console.WriteLine("I am in class method");
+            }
+        }
+
+        // Child or drived class
+        class Child : Parent
+        {
+            // Class member
+            public int childVariable = 99;
+
+            // Class method definition
+            public void childClassMethod()
+            {
+                Console.WriteLine("I am in class method");
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Create an object
+            Child obj = new Child();
+
+            // Access the inheritance
+            Console.WriteLine("Parent Class variable name : " + obj.parentVariable);
+            Console.WriteLine("Child Class variable name : " + obj.childVariable);
+
+            // Access the class method
+            obj.parentClassMethod();
+            obj.childClassMethod();
+        }
+    }
+}
