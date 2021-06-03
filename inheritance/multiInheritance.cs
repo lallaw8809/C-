e3650d78991229f9a1b4655eff54f7dd1e6330ae
lallaw8809 <lal.lawrence@365id.com>
2021-06-03 14:59:51 +0200

    Added a sample code for multi inheritance

---
+++ inheritance/multiInheritance.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace Program
+{
+    class Inheritance
+    {
+        class A
+        {
+            // Class member
+            public string Name = "Lal Bosco";
+
+            // Class method definition
+            public void getName()
+            {
+                Console.WriteLine("Name : " + Name);
+            }
+        }
+
+        class B : A
+        {
+            // Class member
+            public string profession = "Software Developer";
+
+            // Class method definition
+            public void getProfession()
+            {
+                Console.WriteLine("Professtion : {0}", profession);
+            }
+        }
+
+        class C : B
+        {
+            // Class member
+            public string company;
+
+            // Class method definition
+            public void getCompany()
+            {
+                Console.WriteLine("Professtion : {0}", company);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Create an object
+            C obj = new C();
+
+            // Update the company
+            obj.company = "XXXX";
+
+            obj.getName();
+            obj.getProfession();
+            obj.getCompany();
+        }
+    }
+}
